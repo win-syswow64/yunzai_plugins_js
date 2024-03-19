061e9860260a68481d5b5533199318720af14bfd
catboss <64726041+win-syswow64@users.noreply.github.com>
2024-03-19 23:27:19 +0800

    Update 戳一戳（纳西妲版）.js

--- 戳一戳/戳一戳（纳西妲版）.js
+++ 戳一戳/戳一戳（纳西妲版）.js
@@ -7,9 +7,6 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 const path = process.cwd()
 
-//如使用非icqq请在此处填写机器人QQ号
-let BotQQ = ''
-
 // 支持信息详见文件最下方
 //在这里设置事件概率,请保证概率加起来小于1，少于1的部分会触发反击
 let reply_text = 0.4 //文字回复概率
@@ -25,7 +22,7 @@ let lengthScale = 1 //语速
 let sdp_ratio = 0.2 //SDP/DP混合比
 let language = 'ZH'
 let api1url = 'https://api.lolimi.cn/API/yyhc/y.php'
-let api2url = 'https://v2.genshinvoice.top/run/predict'
+let api2url = 'https://bv2.firefly.matce.cn/run/predict'
 let uploadRecord = ""
 let speakerapi1 = "纳西妲" //生成角色api1
 let speakerapi2 = "纳西妲_ZH" //生成角色api2
@@ -147,7 +144,7 @@ export class chuo extends plugin {
     async chuoyichuo(e) {
         if (cfg.masterQQ.includes(e.target_id)) {
             logger.info('[戳主人生效]')
-            if (cfg.masterQQ.includes(e.operator_id) || cfg.qq == e.operator_id || BotQQ == e.operator_id) {
+            if (cfg.masterQQ.includes(e.operator_id) || e.self_id == e.operator_id) {
                 return;
             }
             e.reply([
@@ -159,10 +156,9 @@ export class chuo extends plugin {
             e.group.pokeMember(e.operator_id);
             return true
         }
-        
-        if (e.target_id == cfg.qq || BotQQ == e.target_id) {
+        if (e.target_id == e.self_id) {
             logger.info('[戳一戳生效]')
-            let count = await redis.get(`Yz:pokecount:`);
+            let count = await redis.get(`Yz:pokecount:`);//${e.group_id}
             let usercount = mutetime - 1
             if (mutetime == 0) {
                 usercount = await redis.get('Yz:pokecount' + e.operator_id + ':')
@@ -239,7 +235,7 @@ export class chuo extends plugin {
                     audiourl = responsel.music
                 } else if (ttsapichoose == 'api2') {
                     let data = JSON.stringify({
-                        "data": [`${text}`, `${speakerapi2}`, sdp_ratio, noiseScale, noiseScaleW, lengthScale, `${language}`, null, "Happy", "Text prompt", "", 0.7],
+                        "data": [`${text}`, `${speakerapi2}`, sdp_ratio, noiseScale, noiseScaleW, lengthScale, `${language}`, true, 1, 0.2, null, "Happy", "", "", 0.7],
                         "event_data": null,
                         "fn_index": 0,
                         "session_hash": "v141oxnc02o"
@@ -397,30 +393,7 @@ api2支持角色
 "流浪者_ZH","辛焱_ZH","德沃沙克_ZH","雷电将军_ZH","羽生田千鹤_ZH","那维莱特_ZH","沙扎曼_ZH","纳西妲_ZH","艾伯特_ZH","龙二_ZH","旁白_ZH","克罗索_ZH","元太_ZH","阿贝多_ZH","萍姥姥_ZH",
 "久岐忍_ZH","埃洛伊_ZH","托马_ZH","迪奥娜_ZH","荧_ZH","夏洛蒂_ZH","莱欧斯利_ZH","昆钧_ZH","塞琉斯_ZH","埃泽_ZH","迪娜泽黛_ZH","知易_ZH","玛格丽特_ZH","申鹤_ZH","罗莎莉亚_ZH","娜维娅_ZH",
 "珐露珊_ZH","浮游水蕈兽·元素生命_ZH","奥兹_ZH","砂糖_ZH","绮良良_ZH","杜拉夫_ZH","魈_ZH","松浦_ZH","迈蒙_ZH","荒泷一斗_ZH","吴船长_ZH","埃尔欣根_ZH","柯莱_ZH","阿圆_ZH","「白老先生」_ZH",
-"五郎_ZH","「博士」_ZH","行秋_JP","コナー_JP","籠釣瓶一心_JP","宛煙_JP","アビスの使徒_JP","ティマイオス_JP","タージ·ラドカニ_JP","望雅_JP","イディア_JP","フレミネ_JP","セノ_JP","シャリフ_JP",
-"アデリン_JP","イナヤ_JP","巫女_JP","サーチェン_JP","クンジュ_JP","「淵上」と自称するもの_JP","ニィロウ_JP","シェイクズバイル_JP","式大将_JP","レオン_JP","放浪者_JP","レッシグ_JP","アーラヴ_JP",
-"フェルディナンド_JP","凝光_JP","小倉澪_JP","マーガレット_JP","守_JP","長生_JP","アルバート_JP","慧心_JP","ヨォーヨ_JP","柊千里_JP","丹羽_JP","アルカミ_JP","甘雨_JP","モセイス_JP",
-"掇星攫辰天君_JP","龍二_JP","スクロース_JP","珊瑚宮心海_JP","ヴィハル_JP","マル_JP","スタンレー_JP","御肉丸_JP","上杉_JP","アーロイ_JP","サラ_JP","純水精霊?_JP","嘉良_JP","申鶴_JP",
-"リサ_JP","クリメント_JP","オズ_JP","アルハイゼン_JP","ナヴィア_JP","孟_JP","淑女_JP","傍白_JP","空_JP","古山_JP","七七_JP","サイリュス_JP","ナビル_JP","雷電将軍_JP","九条裟羅_JP",
-"セタレ_JP","天おじ_JP","ロサリア_JP","ドゥラフ_JP","晃_JP","ディオナ_JP","宵宮_JP","テウセル_JP","銀杏_JP","楓原万葉_JP","夜蘭_JP","八重神子_JP","レザー_JP","エルザー_JP","エデ_JP",
-"神里綾人_JP","詩筠_JP","神里綾華_JP","マルシラック_JP","雲菫_JP","ダインスレイヴ_JP","マハールッカデヴァータ_JP","ジン_JP","ノエル_JP","ゴロー_JP","ゾシモス_JP","アイルマン_JP","胡桃_JP",
-"メイモム_JP","アペプ_JP","ベネット_JP","キャンディス_JP","ナーダワ_JP","アイベル_JP","綺良々_JP","ロレンツォ_JP","煙緋_JP","ジョイン_JP","ドニアザード_JP","エルファネ_JP","九条鎌治_JP",
-"バダウィ_JP","クロッサル_JP","鍾離_JP","言笑_JP","「カーブース」_JP","トーマ_JP","エウルア_JP","安西_JP","ウェンティ_JP","プカプカ水キノコン·元素生命_JP","ファルザン_JP","哲平_JP",
-"天目十五_JP","ナヒーダ_JP","一平_JP","アリス_JP","アロイス_JP","リネット_JP","悪龍_JP","アンバー_JP","刻晴_JP","田饒舌_JP","クレー_JP","ラフマン_JP","ラエッド_JP","スカーレット_JP",
-"シコウ_JP","荒瀧一斗_JP","ジェマ_JP","ピンばあや_JP","香菱_JP","つみ_JP","鹿野院平蔵_JP","シラージ_JP","博来_JP","鶯_JP","アザール_JP","パイモン_JP","派蒙_EN","纳西妲_EN","凯亚_EN",
-"阿贝多_EN","温迪_EN","枫原万叶_EN","钟离_EN","荒泷一斗_EN","八重神子_EN","艾尔海森_EN","迪希雅_EN","提纳里_EN","卡维_EN","宵宫_EN","莱依拉_EN","赛诺_EN","诺艾尔_EN","托马_EN","凝光_EN",
-"莫娜_EN","北斗_EN","柯莱_EN","神里绫华_EN","可莉_EN","芭芭拉_EN","雷电将军_EN","珊瑚宫心海_EN","鹿野院平藏_EN","迪奥娜_EN","琴_EN","五郎_EN","班尼特_EN","安柏_EN","夜兰_EN","妮露_EN",
-"辛焱_EN","珐露珊_EN","林尼_EN","丽莎_EN","魈_EN","香菱_EN","烟绯_EN","迪卢克_EN","砂糖_EN","早柚_EN","云堇_EN","刻晴_EN","重云_EN","优菈_EN","胡桃_EN","久岐忍_EN","神里绫人_EN","公子_EN",
-"娜维娅_EN","甘雨_EN","戴因斯雷布_EN","菲谢尔_EN","行秋_EN","白术_EN","九条裟罗_EN","雷泽_EN","申鹤_EN","荧_EN","空_EN","流浪者_EN","迪娜泽黛_EN","凯瑟琳_EN","多莉_EN","坎蒂丝_EN","萍姥姥_EN",
-"罗莎莉亚_EN","埃德_EN","夏洛蒂_EN","伊迪娅_EN","爱贝尔_EN","留云借风真君_EN","散兵_EN","那维莱特_EN","琳妮特_EN","七七_EN","式大将_EN","瑶瑶_EN","奥兹_EN","米卡_EN","达达利亚_EN","哲平_EN",
-"绮良良_EN","浮游水蕈兽·元素生命_EN","大肉丸_EN","托克_EN","蒂玛乌斯_EN","昆钧_EN","欧菲妮_EN","塞琉斯_EN","拉赫曼_EN","阿守_EN","芙宁娜_EN","杜拉夫_EN","伊利亚斯_EN","阿晃_EN","旁白_EN",
-"菲米尼_EN","爱德琳_EN","埃洛伊_EN","迈勒斯_EN","德沃沙克_EN","玛乔丽_EN","塞塔蕾_EN","九条镰治_EN","柊千里_EN","海芭夏_EN","阿娜耶_EN","笼钓瓶一心_EN","回声海螺_EN","元太_EN","阿扎尔_EN",
-"查尔斯_EN","埃勒曼_EN","阿洛瓦_EN","莎拉_EN","纳比尔_EN","康纳_EN","博来_EN","阿祇_EN","玛塞勒_EN","博士_EN","玛格丽特_EN","宛烟_EN","羽生田千鹤_EN","海妮耶_EN","佐西摩斯_EN","霍夫曼_EN",
-"舒伯特_EN","鹿野奈奈_EN","天叔_EN","龙二_EN","艾莉丝_EN","莺儿_EN","嘉良_EN","言笑_EN","费迪南德_EN","珊瑚_EN","嘉玛_EN","久利须_EN","艾文_EN","女士_EN","丹吉尔_EN","白老先生_EN","老孟_EN",
-"天目十五_EN","巴达维_EN","舍利夫_EN","拉齐_EN","吴船长_EN","艾伯特_EN","埃泽_EN","松浦_EN","阿拉夫_EN","莫塞伊思_EN","阿圆_EN","石头_EN","百闻_EN","迈蒙_EN","掇星攫辰天君_EN","博易_EN",
-"斯坦利_EN","毗伽尔_EN","诗筠_EN","慧心_EN","恶龙_EN","小仓澪_EN","知易_EN","恕筠_EN","克列门特_EN","大慈树王_EN","维多利亚_EN","黑田_EN","宁禄_EN","马姆杜_EN","西拉杰_EN","上杉_EN",
-"阿尔卡米_EN","常九爷_EN","纯水精灵_EN","田铁嘴_EN","沙扎曼_EN","加萨尼_EN","克罗索_EN","莱斯格_EN","星稀_EN","阿巴图伊_EN","悦_EN","德田_EN","阿佩普_EN","埃尔欣根_EN","萨赫哈蒂_EN",
-"洛伦佐_EN","深渊使徒_EN","塔杰·拉德卡尼_EN","泽田_EN","安西_EN","理水叠山真君_EN","萨齐因_EN","古田_EN"
+
 支持语言
-ZH:中文   JP:日语    EN：英语
+ZH:中文 
 */
