98cfe7abaadc068fe4a5efe9d56aa0cf59965238
catboss <1004148094@qq.com>
2024-04-22 21:11:12 +0000

    update 戳一戳/戳一戳（纳西妲版）.js.
    Signed-off-by: catboss <1004148094@qq.com>

--- 戳一戳/戳一戳（纳西妲版）.js
+++ 戳一戳/戳一戳（纳西妲版）.js
@@ -1,5 +1,5 @@
-import plugin from '../../lib/plugins/plugin.js'
-import { segment } from 'oicq'
+﻿import plugin from '../../lib/plugins/plugin.js'
+import { segment } from 'icqq'
 import cfg from '../../lib/config/config.js'
 import common from '../../lib/common/common.js'
 import moment from "moment";
@@ -15,7 +15,7 @@ let reply_voice = 0.15 //语音回复概率
 let mutepick = 0.22 //禁言概率
 let example = 0 //拍一拍表情概率
 //剩下的0.08概率就是反击
-let ttsapichoose = 'api1' //api设置
+let ttsapichoose = 'api2' //api设置
 let noiseScale = 0.2  //情感控制
 let noiseScaleW = 0.2 //发音时长
 let lengthScale = 1 //语速
@@ -28,7 +28,7 @@ let speakerapi1 = "纳西妲" //生成角色api1
 let speakerapi2 = "纳西妲_ZH" //生成角色api2
 let text = ""
 let master = "主人"
-let mutetime = 1 //禁言时间设置，单位分钟，如果设置0则为自动递增，如需关闭禁言请修改触发概率为0
+let mutetime = 0 //禁言时间设置，单位分钟，如果设置0则为自动递增，如需关闭禁言请修改触发概率为0
 
 //定义图片存放路径 默认是Yunzai-Bot/resources/chuochuo
 const chuo_path = path + '/resources/chuochuo/';
@@ -129,7 +129,7 @@ export class chuo extends plugin {
             name: '戳一戳',
             dsc: '戳一戳机器人触发效果',
             event: 'notice.group.poke',
-            priority: 5000,
+            priority: 100,
             rule: [
                 {
                     /** 命令正则匹配 */
@@ -158,7 +158,8 @@ export class chuo extends plugin {
         }
         if (e.target_id == e.self_id) {
             logger.info('[戳一戳生效]')
-            let count = await redis.get(`Yz:pokecount:`);//${e.group_id}
+            let count = await redis.get(`Yz:pokecount:`);
+            let group = Bot.pickGroup(`${e.group_id}`,true);
             let usercount = mutetime - 1
             if (mutetime == 0) {
                 usercount = await redis.get('Yz:pokecount' + e.operator_id + ':')
@@ -208,17 +209,24 @@ export class chuo extends plugin {
             //回复随机图片
             else if (random_type < (reply_text + reply_img)) {
                 logger.info('[回复随机图片生效]')
-
-                let photo_number = Math.ceil(Math.random() * (jpg_number + gif_number))
-
-                if (photo_number <= jpg_number) {
-                    e.reply(segment.image(chuo_path + photo_number + '.jpg'))
-                }
-                else {
-                    photo_number = photo_number - jpg_number
-                    e.reply(segment.image(chuo_path + photo_number + '.gif'))
-                }
-
+			  fetch("https://api.xingdream.top/API/poke.php").then(Response => Response.json()).then(data => {
+                        if (data) {
+                            if (data.status == 200) {
+                                try {
+                                    e.reply([segment.image(data.link)])
+                                }
+                                catch (err) {
+                                    e.reply('图片获取失败，请检查网络链接或联系开发者。');
+                                }
+                            }
+                            else {
+                                e.reply(`获取图链失败，错误码：${data.status}`);
+                            }
+                        }
+                        else {
+                            e.reply('图片api异常。');
+                        }
+                    })
             }
 
             //回复随机语音
@@ -284,6 +292,26 @@ export class chuo extends plugin {
             }
             //禁言
             else if (random_type < (reply_text + reply_img + reply_voice + mutepick)) {
+            	if(cfg.masterQQ.includes(e.operator_id) || !group.is_admin){
+            		fetch("https://api.xingdream.top/API/poke.php").then(Response => Response.json()).then(data => {
+                        if (data) {
+                            if (data.status == 200) {
+                                try {
+                                    e.reply([segment.image(data.link)])
+                                }
+                                catch (err) {
+                                    e.reply('图片获取失败，请检查网络链接或联系开发者。');
+                                }
+                            }
+                            else {
+                                e.reply(`获取图链失败，错误码：${data.status}`);
+                            }
+                        }
+                        else {
+                            e.reply('图片api异常。');
+                        }
+                    })
+            	}
                 logger.info('[禁言生效]')
                 logger.info(e.operator_id + `将要被禁言${usercount + 1}分钟`)
                 if (usercount >= 36) {
@@ -358,65 +386,3 @@ export class chuo extends plugin {
     }
 
 }
-/*更新日志：
-V1.0 Bate 0.1 10.23 修复了图片发送与语音合成。
-V1.1 Bate 0.1 10.29 增加了戳主人相关内容。
-V1.1 Bate 0.2 11.01 修复了语音API失效问题
-V1.1 Bate 0.3 11.06 适配新版语音API
-V1.1 Bate 0.4 11.17 修复了机器人戳主人会触发戳主人功能的BUG
-V1.2 Bate 0.1 12.14 1.适配了新的语音API。2.语音API支持双语音API
-V1.2 Bate 0.1 12.19 1.修复语音API。2.新增更多DeBUG信息。
-*/
-/*
-api1支持角色
-空, 荧, 派蒙, 纳西妲, 阿贝多, 温迪, 枫原万叶, 钟离, 荒泷一斗, 八重神子, 艾尔海森, 提纳里, 迪希雅, 卡维, 宵宫, 莱依拉, 赛诺, 诺艾尔, 托马, 凝光, 莫娜, 北斗, 神里绫华, 雷电将军, 
-芭芭拉, 鹿野院平藏, 五郎, 迪奥娜, 凯亚, 安柏, 班尼特, 琴, 柯莱, 夜兰, 妮露, 辛焱, 珐露珊, 魈, 香菱, 达达利亚, 砂糖, 早柚, 云堇, 刻晴, 丽莎, 迪卢克, 烟绯, 重云, 珊瑚宫心海, 胡桃,
-可莉, 流浪者, 久岐忍, 神里绫人, 甘雨, 戴因斯雷布, 优菈, 菲谢尔, 行秋, 白术, 九条裟罗, 雷泽, 申鹤, 迪娜泽黛, 凯瑟琳, 多莉, 坎蒂丝, 萍姥姥, 罗莎莉亚, 留云借风真君, 绮良良, 瑶瑶, 七七, 
-奥兹, 米卡, 夏洛蒂, 埃洛伊, 博士, 女士, 大慈树王, 三月七, 娜塔莎, 希露瓦, 虎克, 克拉拉, 丹恒, 希儿, 布洛妮娅, 瓦尔特, 杰帕德, 佩拉, 姬子, 艾丝妲, 白露, 星, 穹, 桑博, 伦纳德, 停云, 罗刹, 
-卡芙卡, 彦卿, 史瓦罗, 螺丝咕姆, 阿兰, 银狼, 素裳, 丹枢, 黑塔, 景元, 帕姆, 可可利亚, 半夏, 符玄, 公输师傅, 奥列格, 青雀, 大毫, 青镞, 费斯曼, 绿芙蓉, 镜流, 信使, 丽塔, 失落迷迭, 缭乱星棘,
- 伊甸, 伏特加女孩, 狂热蓝调, 莉莉娅, 萝莎莉娅, 八重樱, 八重霞, 卡莲, 第六夜想曲, 卡萝尔, 姬子, 极地战刃, 布洛妮娅, 次生银翼, 理之律者, 真理之律者, 迷城骇兔, 希儿, 魇夜星渊, 黑希儿, 
- 帕朵菲莉丝, 天元骑英, 幽兰黛尔, 德丽莎, 月下初拥, 朔夜观星, 暮光骑士, 明日香, 李素裳, 格蕾修, 梅比乌斯, 渡鸦, 人之律者, 爱莉希雅, 爱衣, 天穹游侠, 琪亚娜, 空之律者, 终焉之律者, 薪炎之律者,
-  云墨丹心, 符华, 识之律者, 维尔薇, 始源之律者, 芽衣, 雷之律者, 苏莎娜, 阿波尼亚, 陆景和, 莫弈, 夏彦, 左然
-*/
-/*
-api2支持角色
-"埃德_ZH","塔杰·拉德卡尼_ZH","行秋_ZH","深渊使徒_ZH","凯瑟琳_ZH","常九爷_ZH","神里绫人_ZH","丽莎_ZH","纯水精灵?_ZH","宛烟_ZH","重云_ZH","悦_ZH","莱依拉_ZH","鹿野奈奈_ZH",
-"式大将_ZH","白术_ZH","埃舍尔_ZH","莫娜_ZH","优菈_ZH","琴_ZH","凯亚_ZH","西拉杰_ZH","凝光_ZH","石头_ZH","达达利亚_ZH","伊利亚斯_ZH","艾尔海森_ZH","慧心_ZH","「大肉丸」_ZH",
-"柊千里_ZH","玛乔丽_ZH","神里绫华_ZH","菲米尼_ZH","甘雨_ZH","掇星攫辰天君_ZH","坎蒂丝_ZH","上杉_ZH","阿尔卡米_ZH","戴因斯雷布_ZH","艾文_ZH","回声海螺_ZH","九条裟罗_ZH",
-"迪卢克_ZH","提纳里_ZH","嘉良_ZH","塞塔蕾_ZH","琳妮特_ZH","阿洛瓦_ZH","蒂玛乌斯_ZH","枫原万叶_ZH","丹吉尔_ZH","空_ZH","林尼_ZH","阿守_ZH","七七_ZH","嘉玛_ZH","恶龙_ZH",
-"阿巴图伊_ZH","阿佩普_ZH","八重神子_ZH","迪希雅_ZH","迈勒斯_ZH","夜兰_ZH","萨赫哈蒂_ZH","欧菲妮_ZH","笼钓瓶一心_ZH","芭芭拉_ZH","瑶瑶_ZH","天叔_ZH","派蒙_ZH","米卡_ZH",
-"玛塞勒_ZH","胡桃_ZH","百闻_ZH","艾莉丝_ZH","安柏_ZH","阿晃_ZH","萨齐因_ZH","田铁嘴_ZH","烟绯_ZH","海妮耶_ZH","纳比尔_ZH","女士_ZH","诺艾尔_ZH","云堇_ZH","舒伯特_ZH",
-"埃勒曼_ZH","九条镰治_ZH","留云借风真君_ZH","言笑_ZH","安西_ZH","珊瑚宫心海_ZH","托克_ZH","哲平_ZH","恕筠_ZH","拉赫曼_ZH","久利须_ZH","天目十五_ZH","妮露_ZH","莺儿_ZH",
-"佐西摩斯_ZH","鹿野院平藏_ZH","温迪_ZH","菲谢尔_ZH","anzai_ZH","可莉_ZH","刻晴_ZH","克列门特_ZH","阿扎尔_ZH","班尼特_ZH","伊迪娅_ZH","巴达维_ZH","深渊法师_ZH","赛诺_ZH",
-"大慈树王_ZH","拉齐_ZH","海芭夏_ZH","香菱_ZH","康纳_ZH","阿祇_ZH","卡维_ZH","博来_ZH","斯坦利_ZH","霍夫曼_ZH","北斗_ZH","阿拉夫_ZH","陆行岩本真蕈·元素生命_ZH","爱贝尔_ZH",
-"雷泽_ZH","毗伽尔_ZH","莎拉_ZH","莫塞伊思_ZH","多莉_ZH","珊瑚_ZH","老孟_ZH","宵宫_ZH","钟离_ZH","芙宁娜_ZH","爱德琳_ZH","「女士」_ZH","博易_ZH","长生_ZH","查尔斯_ZH","阿娜耶_ZH",
-"流浪者_ZH","辛焱_ZH","德沃沙克_ZH","雷电将军_ZH","羽生田千鹤_ZH","那维莱特_ZH","沙扎曼_ZH","纳西妲_ZH","艾伯特_ZH","龙二_ZH","旁白_ZH","克罗索_ZH","元太_ZH","阿贝多_ZH","萍姥姥_ZH",
-"久岐忍_ZH","埃洛伊_ZH","托马_ZH","迪奥娜_ZH","荧_ZH","夏洛蒂_ZH","莱欧斯利_ZH","昆钧_ZH","塞琉斯_ZH","埃泽_ZH","迪娜泽黛_ZH","知易_ZH","玛格丽特_ZH","申鹤_ZH","罗莎莉亚_ZH","娜维娅_ZH",
-"珐露珊_ZH","浮游水蕈兽·元素生命_ZH","奥兹_ZH","砂糖_ZH","绮良良_ZH","杜拉夫_ZH","魈_ZH","松浦_ZH","迈蒙_ZH","荒泷一斗_ZH","吴船长_ZH","埃尔欣根_ZH","柯莱_ZH","阿圆_ZH","「白老先生」_ZH",
-"五郎_ZH","「博士」_ZH","行秋_JP","コナー_JP","籠釣瓶一心_JP","宛煙_JP","アビスの使徒_JP","ティマイオス_JP","タージ·ラドカニ_JP","望雅_JP","イディア_JP","フレミネ_JP","セノ_JP","シャリフ_JP",
-"アデリン_JP","イナヤ_JP","巫女_JP","サーチェン_JP","クンジュ_JP","「淵上」と自称するもの_JP","ニィロウ_JP","シェイクズバイル_JP","式大将_JP","レオン_JP","放浪者_JP","レッシグ_JP","アーラヴ_JP",
-"フェルディナンド_JP","凝光_JP","小倉澪_JP","マーガレット_JP","守_JP","長生_JP","アルバート_JP","慧心_JP","ヨォーヨ_JP","柊千里_JP","丹羽_JP","アルカミ_JP","甘雨_JP","モセイス_JP",
-"掇星攫辰天君_JP","龍二_JP","スクロース_JP","珊瑚宮心海_JP","ヴィハル_JP","マル_JP","スタンレー_JP","御肉丸_JP","上杉_JP","アーロイ_JP","サラ_JP","純水精霊?_JP","嘉良_JP","申鶴_JP",
-"リサ_JP","クリメント_JP","オズ_JP","アルハイゼン_JP","ナヴィア_JP","孟_JP","淑女_JP","傍白_JP","空_JP","古山_JP","七七_JP","サイリュス_JP","ナビル_JP","雷電将軍_JP","九条裟羅_JP",
-"セタレ_JP","天おじ_JP","ロサリア_JP","ドゥラフ_JP","晃_JP","ディオナ_JP","宵宮_JP","テウセル_JP","銀杏_JP","楓原万葉_JP","夜蘭_JP","八重神子_JP","レザー_JP","エルザー_JP","エデ_JP",
-"神里綾人_JP","詩筠_JP","神里綾華_JP","マルシラック_JP","雲菫_JP","ダインスレイヴ_JP","マハールッカデヴァータ_JP","ジン_JP","ノエル_JP","ゴロー_JP","ゾシモス_JP","アイルマン_JP","胡桃_JP",
-"メイモム_JP","アペプ_JP","ベネット_JP","キャンディス_JP","ナーダワ_JP","アイベル_JP","綺良々_JP","ロレンツォ_JP","煙緋_JP","ジョイン_JP","ドニアザード_JP","エルファネ_JP","九条鎌治_JP",
-"バダウィ_JP","クロッサル_JP","鍾離_JP","言笑_JP","「カーブース」_JP","トーマ_JP","エウルア_JP","安西_JP","ウェンティ_JP","プカプカ水キノコン·元素生命_JP","ファルザン_JP","哲平_JP",
-"天目十五_JP","ナヒーダ_JP","一平_JP","アリス_JP","アロイス_JP","リネット_JP","悪龍_JP","アンバー_JP","刻晴_JP","田饒舌_JP","クレー_JP","ラフマン_JP","ラエッド_JP","スカーレット_JP",
-"シコウ_JP","荒瀧一斗_JP","ジェマ_JP","ピンばあや_JP","香菱_JP","つみ_JP","鹿野院平蔵_JP","シラージ_JP","博来_JP","鶯_JP","アザール_JP","パイモン_JP","派蒙_EN","纳西妲_EN","凯亚_EN",
-"阿贝多_EN","温迪_EN","枫原万叶_EN","钟离_EN","荒泷一斗_EN","八重神子_EN","艾尔海森_EN","迪希雅_EN","提纳里_EN","卡维_EN","宵宫_EN","莱依拉_EN","赛诺_EN","诺艾尔_EN","托马_EN","凝光_EN",
-"莫娜_EN","北斗_EN","柯莱_EN","神里绫华_EN","可莉_EN","芭芭拉_EN","雷电将军_EN","珊瑚宫心海_EN","鹿野院平藏_EN","迪奥娜_EN","琴_EN","五郎_EN","班尼特_EN","安柏_EN","夜兰_EN","妮露_EN",
-"辛焱_EN","珐露珊_EN","林尼_EN","丽莎_EN","魈_EN","香菱_EN","烟绯_EN","迪卢克_EN","砂糖_EN","早柚_EN","云堇_EN","刻晴_EN","重云_EN","优菈_EN","胡桃_EN","久岐忍_EN","神里绫人_EN","公子_EN",
-"娜维娅_EN","甘雨_EN","戴因斯雷布_EN","菲谢尔_EN","行秋_EN","白术_EN","九条裟罗_EN","雷泽_EN","申鹤_EN","荧_EN","空_EN","流浪者_EN","迪娜泽黛_EN","凯瑟琳_EN","多莉_EN","坎蒂丝_EN","萍姥姥_EN",
-"罗莎莉亚_EN","埃德_EN","夏洛蒂_EN","伊迪娅_EN","爱贝尔_EN","留云借风真君_EN","散兵_EN","那维莱特_EN","琳妮特_EN","七七_EN","式大将_EN","瑶瑶_EN","奥兹_EN","米卡_EN","达达利亚_EN","哲平_EN",
-"绮良良_EN","浮游水蕈兽·元素生命_EN","大肉丸_EN","托克_EN","蒂玛乌斯_EN","昆钧_EN","欧菲妮_EN","塞琉斯_EN","拉赫曼_EN","阿守_EN","芙宁娜_EN","杜拉夫_EN","伊利亚斯_EN","阿晃_EN","旁白_EN",
-"菲米尼_EN","爱德琳_EN","埃洛伊_EN","迈勒斯_EN","德沃沙克_EN","玛乔丽_EN","塞塔蕾_EN","九条镰治_EN","柊千里_EN","海芭夏_EN","阿娜耶_EN","笼钓瓶一心_EN","回声海螺_EN","元太_EN","阿扎尔_EN",
-"查尔斯_EN","埃勒曼_EN","阿洛瓦_EN","莎拉_EN","纳比尔_EN","康纳_EN","博来_EN","阿祇_EN","玛塞勒_EN","博士_EN","玛格丽特_EN","宛烟_EN","羽生田千鹤_EN","海妮耶_EN","佐西摩斯_EN","霍夫曼_EN",
-"舒伯特_EN","鹿野奈奈_EN","天叔_EN","龙二_EN","艾莉丝_EN","莺儿_EN","嘉良_EN","言笑_EN","费迪南德_EN","珊瑚_EN","嘉玛_EN","久利须_EN","艾文_EN","女士_EN","丹吉尔_EN","白老先生_EN","老孟_EN",
-"天目十五_EN","巴达维_EN","舍利夫_EN","拉齐_EN","吴船长_EN","艾伯特_EN","埃泽_EN","松浦_EN","阿拉夫_EN","莫塞伊思_EN","阿圆_EN","石头_EN","百闻_EN","迈蒙_EN","掇星攫辰天君_EN","博易_EN",
-"斯坦利_EN","毗伽尔_EN","诗筠_EN","慧心_EN","恶龙_EN","小仓澪_EN","知易_EN","恕筠_EN","克列门特_EN","大慈树王_EN","维多利亚_EN","黑田_EN","宁禄_EN","马姆杜_EN","西拉杰_EN","上杉_EN",
-"阿尔卡米_EN","常九爷_EN","纯水精灵_EN","田铁嘴_EN","沙扎曼_EN","加萨尼_EN","克罗索_EN","莱斯格_EN","星稀_EN","阿巴图伊_EN","悦_EN","德田_EN","阿佩普_EN","埃尔欣根_EN","萨赫哈蒂_EN",
-"洛伦佐_EN","深渊使徒_EN","塔杰·拉德卡尼_EN","泽田_EN","安西_EN","理水叠山真君_EN","萨齐因_EN","古田_EN"
-支持语言
-ZH:中文   JP:日语    EN：英语
-*/
